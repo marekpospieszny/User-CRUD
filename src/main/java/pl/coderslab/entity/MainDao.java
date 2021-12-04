b616bce71370285fee0515608ba382d4e319e56e
Marek <marek.pospieszny@gmail.com>
2021-12-04 14:38:40 +0100

    UserDao methods created

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -2,6 +2,8 @@ package pl.coderslab.entity;
 
 import pl.coderslab.entity.User;
 
+import java.util.Arrays;
+
 public class MainDao {
 
     public static void main(String[] args) {
@@ -25,17 +27,39 @@ public class MainDao {
 //        userDao.create(user2);
 //        System.out.println(user2.getId());
 
-        User user3 = userDao.read(5);
-        System.out.println(user3.toString());
+//        Pobieranie danych:
 
-        User user4 = userDao.read(6);
-        System.out.println(user4.toString());
+//        User user3 = userDao.read(5);
+//        System.out.println(user3.toString());
+//
+//        User user4 = userDao.read(6);
+//        System.out.println(user4.toString());
+//
+//        User user5 = userDao.read("test2@wp.pl");
+//        System.out.println(user5.toString());
+//
+//        User user6 = userDao.read("test@test.pl");
+//        System.out.println(user6.toString());
 
-        User user5 = userDao.read("test2@wp.pl");
-        System.out.println(user5.toString());
+//        Update data:
 
-        User user6 = userDao.read("test@test.pl");
-        System.out.println(user6.toString());
+//        User user7 = userDao.read(2);
+//        user7.setEmail("updatedAgain@test.pl");
+//        user7.setUserName("updatedUser");
+//        user7.setPassword("updatedPassword");
+//        userDao.update(user7);
+
+//        Delete data:
+
+//        userDao.delete(3);
+
+//        User user8 = new User();
+//        user8.setEmail("email@email.pl");
+//        user8.setUserName("Username");
+//        user8.setPassword("Password");
+//        userDao.create(user8);
+
+        System.out.println(Arrays.toString(userDao.findAll()));
 
 
     }
--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -4,6 +4,7 @@ import pl.coderslab.workshop2.BCrypt;
 import pl.coderslab.workshop2.DBUtil;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao extends User {
 
@@ -11,6 +12,7 @@ public class UserDao extends User {
     private static final String READ_ALL_USERS_QUERY = "SELECT * FROM users";
     private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
     private static final String READ_USER_EMAIL_QUERY = "SELECT * FROM users WHERE email = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
     private static final String UPDATE_EMAIL_QUERY = "UPDATE users SET email = ? WHERE id = ?";
     private static final String UPDATE_USERNAME_QUERY = "UPDATE users SET username = ? WHERE id = ?";
     private static final String UPDATE_PASSWORD_QUERY = "UPDATE users SET password = ? WHERE id = ?";
@@ -24,8 +26,8 @@ public class UserDao extends User {
         try (Connection conn = DBUtil.getConnection()) {
             PreparedStatement statement =
                     conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, user.getUserName());
-            statement.setString(2, user.getEmail());
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUserName());
             statement.setString(3, hashPassword(user.getPassword()));
             statement.executeUpdate();
             //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
@@ -35,6 +37,7 @@ public class UserDao extends User {
             }
             return user;
         } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
             e.printStackTrace();
             return null;
         }
@@ -54,6 +57,7 @@ public class UserDao extends User {
             }
             return user;
         } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
             return null;
         }
     }
@@ -72,8 +76,65 @@ public class UserDao extends User {
             }
             return user;
         } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
             return null;
         }
     }
+
+    public void update(User user) {
+        try (Connection connection = DBUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1,user.getEmail());
+            statement.setString(2,user.getUserName());
+            statement.setString(3,hashPassword(user.getPassword()));
+            statement.setInt(4,user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
+        }
+
+    }
+
+    public void delete(int userId) {
+        try (Connection connection = DBUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(DELETE_USER_QUERY);
+            statement.setInt(1,userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
+        }
+    }
+
+//    SELECT * FROM users
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Tworzymy kopię tablicy powiększoną o 1.
+        tmpUsers[users.length] = u; // Dodajemy obiekt na ostatniej pozycji.
+        return tmpUsers; // Zwracamy nową tablicę.
+    }
+
+    public User[] findAll () {
+        try (Connection connection = DBUtil.getConnection()) {
+            User[] allUsers = new User[0];
+            PreparedStatement statement = connection.prepareStatement(READ_ALL_USERS_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt(1));
+                user.setEmail(resultSet.getString(2));
+                user.setUserName(resultSet.getString(3));
+                user.setPassword(resultSet.getString(4));
+                allUsers = addToArray(user,allUsers);
+            }
+            return allUsers;
+        } catch (SQLException e) {
+            System.out.println("Unable to establish connection with database.");
+            return null;
+        }
+
+    }
+
+
+
 
 }
